1ff55c62b79fcee226a624ca645eb952263e1800
GregoryI <iwanekgregory@gmail.com>
2016-11-24 21:07:09 +0100

    24.11.2016

--- app/src/main/java/pl/grzegorziwanek/altimeter/app/MainFragment.java
+++ app/src/main/java/pl/grzegorziwanek/altimeter/app/MainFragment.java
@@ -1,6 +1,7 @@
 package pl.grzegorziwanek.altimeter.app;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -16,8 +17,19 @@ public class MainFragment extends Fragment
     public MainFragment(){}
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+    }
+
+    //consist actions to perform upon re/start of app ( update current location and information)
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        //refresh info on screen on app start/restart
+        updateAppInfo();
     }
 
     @Override
@@ -26,5 +38,21 @@ public class MainFragment extends Fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         return rootView;
+    }
+
+    //called onStart and restart-> update information to show on app start
+    private void updateAppInfo()
+    {
+
+    }
+
+    //inner class responsible for background update
+    //ASyncTask <params, progress, result> -> params: given entry data to work on; progress: data to show progress; result: result of background execution
+    public class FetchDataInfoTask extends AsyncTask<String, Void, String[]>
+    {
+        @Override
+        protected String[] doInBackground(String... strings) {
+            return new String[0];
+        }
     }
 }
